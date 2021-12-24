e68e34caa67bed51d3f3a0c16d50a68fd17fd38c
S M Ashraful Azom <ashrafulazom@gmail.com>
2021-12-24 19:01:23 +0600

    Update jsnewscode28112021.js

--- js/jsnewscode28112021.js
+++ js/jsnewscode28112021.js
@@ -79,6 +79,18 @@ posts+='</ul>';document.getElementById('readers').innerHTML=posts;}
 function feature(json){var posts="<ul class=mbtlist>";for(var i=0;i<json.feed.entry.length;i++){var title=json.feed.entry[i].title.$t;var url=json.feed.entry[i].link.pop().href;var thumbnail=json.feed.entry[i].media$thumbnail.url;if(json.feed.entry[i].media$thumbnail){thumbnail=thumbnail.replace("/s72-c/","/s"+80+"/");}else{thumbnail="https://cdn.jsdelivr.net/gh/ashrafulazom/sebahotnews/img/seba-200.webp";}
 posts+="<li><a class='mbttitle' href='"+url+"'><img src='"+thumbnail+"' loading='lazy' alt='"+title+"'>"+title+"</a></li>";}
 posts+='</ul>';document.getElementById('feature').innerHTML=posts;}
+function expatriation(json){var posts="<ul class=mbtlist>";for(var i=0;i<json.feed.entry.length;i++){var title=json.feed.entry[i].title.$t;var url=json.feed.entry[i].link.pop().href;var thumbnail=json.feed.entry[i].media$thumbnail.url;if(json.feed.entry[i].media$thumbnail){thumbnail=thumbnail.replace("/s72-c/","/s"+80+"/");}else{thumbnail="https://cdn.jsdelivr.net/gh/ashrafulazom/sebahotnews/img/seba-200.webp";}
+posts+="<li><a class='mbttitle' href='"+url+"'><img src='"+thumbnail+"' loading='lazy' alt='"+title+"'>"+title+"</a></li>";}
+posts+='</ul>';document.getElementById('expatriation').innerHTML=posts;}
+function trade(json){var posts="<ul class=mbtlist>";for(var i=0;i<json.feed.entry.length;i++){var title=json.feed.entry[i].title.$t;var url=json.feed.entry[i].link.pop().href;var thumbnail=json.feed.entry[i].media$thumbnail.url;if(json.feed.entry[i].media$thumbnail){thumbnail=thumbnail.replace("/s72-c/","/s"+80+"/");}else{thumbnail="https://cdn.jsdelivr.net/gh/ashrafulazom/sebahotnews/img/seba-200.webp";}
+posts+="<li><a class='mbttitle' href='"+url+"'><img src='"+thumbnail+"' loading='lazy' alt='"+title+"'>"+title+"</a></li>";}
+posts+='</ul>';document.getElementById('trade').innerHTML=posts;}
+function fighters(json){var posts="<ul class=mbtlist>";for(var i=0;i<json.feed.entry.length;i++){var title=json.feed.entry[i].title.$t;var url=json.feed.entry[i].link.pop().href;var thumbnail=json.feed.entry[i].media$thumbnail.url;if(json.feed.entry[i].media$thumbnail){thumbnail=thumbnail.replace("/s72-c/","/s"+80+"/");}else{thumbnail="https://cdn.jsdelivr.net/gh/ashrafulazom/sebahotnews/img/seba-200.webp";}
+posts+="<li><a class='mbttitle' href='"+url+"'><img src='"+thumbnail+"' loading='lazy' alt='"+title+"'>"+title+"</a></li>";}
+posts+='</ul>';document.getElementById('fighters').innerHTML=posts;}
+function personality(json){var posts="<ul class=mbtlist>";for(var i=0;i<json.feed.entry.length;i++){var title=json.feed.entry[i].title.$t;var url=json.feed.entry[i].link.pop().href;var thumbnail=json.feed.entry[i].media$thumbnail.url;if(json.feed.entry[i].media$thumbnail){thumbnail=thumbnail.replace("/s72-c/","/s"+80+"/");}else{thumbnail="https://cdn.jsdelivr.net/gh/ashrafulazom/sebahotnews/img/seba-200.webp";}
+posts+="<li><a class='mbttitle' href='"+url+"'><img src='"+thumbnail+"' loading='lazy' alt='"+title+"'>"+title+"</a></li>";}
+posts+='</ul>';document.getElementById('personality').innerHTML=posts;}
 function listlatH(json){var posts="<ul class=mbtlistlat>";for(var i=1;i<json.feed.entry.length;i++){var title=json.feed.entry[i].title.$t;var url=json.feed.entry[i].link.pop().href;posts+="<li><a href='"+url+"'>"+title+"</a></li>";}
 posts+='</ul>';document.getElementById('listlatH').innerHTML=posts;}
 function listlatP(json){var posts="<ul class=mbtlistlat>";for(var i=0;i<json.feed.entry.length;i++){var title=json.feed.entry[i].title.$t;var url=json.feed.entry[i].link.pop().href;posts+="<li><a href='"+url+"'>"+title+"</a></li>";}

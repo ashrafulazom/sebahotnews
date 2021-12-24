89806b591fd2eaf69c565132bc1f4c89e048bbb3
S M Ashraful Azom <ashrafulazom@gmail.com>
2021-12-24 18:20:07 +0600

    Update jsnewscode28112021.js
    Add to Cattegory "Low and Election"

--- js/jsnewscode28112021.js
+++ js/jsnewscode28112021.js
@@ -49,6 +49,12 @@ posts+='</ul>';document.getElementById('editor').innerHTML=posts;}
 function lifestyle(json){var posts="<ul class=mbtlist>";for(var i=0;i<json.feed.entry.length;i++){var title=json.feed.entry[i].title.$t;var url=json.feed.entry[i].link.pop().href;var thumbnail=json.feed.entry[i].media$thumbnail.url;if(json.feed.entry[i].media$thumbnail){thumbnail=thumbnail.replace("/s72-c/","/s"+80+"/");}else{thumbnail="https://cdn.jsdelivr.net/gh/ashrafulazom/sebahotnews/img/seba-200.webp";}
 posts+="<li><a class='mbttitle' href='"+url+"'><img src='"+thumbnail+"' loading='lazy' alt='"+title+"'>"+title+"</a></li>";}
 posts+='</ul>';document.getElementById('lifestyle').innerHTML=posts;}
+function low(json){var posts="<ul class=mbtlist>";for(var i=0;i<json.feed.entry.length;i++){var title=json.feed.entry[i].title.$t;var url=json.feed.entry[i].link.pop().href;var thumbnail=json.feed.entry[i].media$thumbnail.url;if(json.feed.entry[i].media$thumbnail){thumbnail=thumbnail.replace("/s72-c/","/s"+80+"/");}else{thumbnail="https://cdn.jsdelivr.net/gh/ashrafulazom/sebahotnews/img/seba-200.webp";}
+posts+="<li><a class='mbttitle' href='"+url+"'><img src='"+thumbnail+"' loading='lazy' alt='"+title+"'>"+title+"</a></li>";}
+posts+='</ul>';document.getElementById('low').innerHTML=posts;}
+function election(json){var posts="<ul class=mbtlist>";for(var i=0;i<json.feed.entry.length;i++){var title=json.feed.entry[i].title.$t;var url=json.feed.entry[i].link.pop().href;var thumbnail=json.feed.entry[i].media$thumbnail.url;if(json.feed.entry[i].media$thumbnail){thumbnail=thumbnail.replace("/s72-c/","/s"+80+"/");}else{thumbnail="https://cdn.jsdelivr.net/gh/ashrafulazom/sebahotnews/img/seba-200.webp";}
+posts+="<li><a class='mbttitle' href='"+url+"'><img src='"+thumbnail+"' loading='lazy' alt='"+title+"'>"+title+"</a></li>";}
+posts+='</ul>';document.getElementById('election').innerHTML=posts;}
 function listlatH(json){var posts="<ul class=mbtlistlat>";for(var i=1;i<json.feed.entry.length;i++){var title=json.feed.entry[i].title.$t;var url=json.feed.entry[i].link.pop().href;posts+="<li><a href='"+url+"'>"+title+"</a></li>";}
 posts+='</ul>';document.getElementById('listlatH').innerHTML=posts;}
 function listlatP(json){var posts="<ul class=mbtlistlat>";for(var i=0;i<json.feed.entry.length;i++){var title=json.feed.entry[i].title.$t;var url=json.feed.entry[i].link.pop().href;posts+="<li><a href='"+url+"'>"+title+"</a></li>";}
